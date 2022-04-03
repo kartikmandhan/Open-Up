c5cdaf695c1dbc73c4d43100bcc8b0cf45045026
Kartik <kartikmandhan14@gmail.com>
2022-04-03 16:09:04 +0530

    CommentPage component Created

---
+++ frontend/src/components/CommentPage.jsx
@@ -0,0 +1,50 @@
+import { SocialContext } from "../context/SocialContext";
+import React, { useContext } from "react";
+import { useMoralisQuery } from "react-moralis";
+import Sidebar from "./Sidebar/Sidebar";
+import Post from "./Feed/Post";
+import PostInput from "./Feed/PostInput";
+const CommentPage = (props) => {
+  const queryCategories = useMoralisQuery("Categories");
+  const fetchedCategories = JSON.parse(
+    JSON.stringify(queryCategories.data, ["categoryId", "category"])
+  );
+  const post = props.location.state;
+  const queryPost = useMoralisQuery(
+    "Posts",
+    (query) => query.equalTo("parentId", post?.parentId),
+    [post],
+    { live: true }
+  );
+  const fetchedPosts = JSON.parse(
+    JSON.stringify(queryPost.data, ["postId", "contentId", "postOwner"])
+  ).reverse();
+  console.log(fetchedPosts);
+  return (
+    <div className="container">
+      <div
+        style={{
+          display: "flex",
+          fontFamily: "Roboto, sans-serif",
+          color: "#041836",
+          padding: "10px 30px",
+          maxWidth: "1200px",
+          width: "100%",
+          gap: "20px",
+        }}
+      >
+        <Sidebar categories={fetchedCategories} />
+        <div style={{ flexDirection: "column" }}>
+          <Post post={post} commentIconVisible={false} />
+          <PostInput parentId={post?.parentId} />
+          {fetchedPosts?.length > 0 &&
+            fetchedPosts.map((post) => (
+              <Post key={post?.postId} post={post} commentIconVisible={false} />
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CommentPage;
